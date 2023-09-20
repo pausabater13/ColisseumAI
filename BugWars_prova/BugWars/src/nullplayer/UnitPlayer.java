e38f1e04f99a950eb487af0b50b34afcedc49972
AlexRG03 <alex.rodriguez.garcia@estudiantat.upc.edu>
2023-09-20 15:49:59 +0200

    push BugWars

---
+++ BugWars_prova/BugWars/src/nullplayer/UnitPlayer.java
@@ -0,0 +1,16 @@
+package nullplayer;
+
+import bugwars.user.*;
+
+public class UnitPlayer {
+
+	public void run(UnitController uc) {
+		// Code to be executed only at the beginning of the unit's lifespan
+
+		while (true) {
+			// Code to be executed every round
+			uc.yield(); // End of turn
+		}
+	}
+
+}
