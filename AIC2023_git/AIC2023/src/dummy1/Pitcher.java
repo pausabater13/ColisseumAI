890cf6f3ee7bb37179c6070b136e2a444e471ca4
Pau Sabater Nácher <pau.sabnac@gmail.com>
2023-09-27 10:04:06 +0200

    Pitcher update

--- AIC2023_git/AIC2023/src/dummy1/Pitcher.java
+++ AIC2023_git/AIC2023/src/dummy1/Pitcher.java
@@ -28,63 +28,70 @@ public class Pitcher extends MyUnit {
         Direction dir = Direction.values()[randomNumberDir];
         int k = (int)(Math.random()*10);
 
-
         Location target = getClosestBaseOrStadium(uc);
 
-        Direction rUnitDir = unitDirection.rotateRight();
-        Direction rrUnitDir = rUnitDir.rotateRight();
-        Direction rrrUnitDir = rrUnitDir.rotateRight();
-        Direction lUnitDir = unitDirection.rotateLeft();
-        Direction llUnitDir = lUnitDir.rotateLeft();
-        Direction lllUnitDir = llUnitDir.rotateLeft();
-
-        if (target != null) {
+        Direction checkedDir;
+        //If sees an agresive unit: RUN!!
+        if (closeBatters(uc) != null) checkedDir = goDirection(closeBatters(uc));
+            //First option: bases or stadiums
+        else if (target != null) {
             /*Try to get closer to the target */
             Direction targetDir = uc.getLocation().directionTo(target);
-
-            //If sees an agresive unit: RUN!!
-            
-            if (closeBatters(uc) != null) uc.move(closeBatters(uc));
-            //First option: bases or stadiums
-            if (uc.canMove(targetDir)) uc.move(targetDir);
-            //Second options: 1/4 probability of turning 45º direction to left or right
-            else if (((int)(Math.random()*4)==1) || (!uc.canMove(unitDirection))){
-                if (uc.canMove(rUnitDir) && (int)(Math.random()*2)==1) {uc.move(rUnitDir);unitDirection=rUnitDir;}
-                else if (uc.canMove(lUnitDir)) {uc.move(lUnitDir);unitDirection=lUnitDir;}
-
-                //If not able to go: turn right or left 90º
-                else if ((int)(Math.random()*2)==1 && uc.canMove(rrUnitDir)) {uc.move(rrUnitDir);unitDirection=rrUnitDir;}
-                else if ((int)(Math.random()*2)==1 && uc.canMove(llUnitDir)) {uc.move(llUnitDir);unitDirection=llUnitDir;}
-
-                //If not able to go: turn right or left º
-                else if ((int)(Math.random()*2)==1 && uc.canMove(rrrUnitDir)) {uc.move(rrrUnitDir);unitDirection=rrrUnitDir;}
-                else if ((int)(Math.random()*2)==1 && uc.canMove(lllUnitDir)) {uc.move(lllUnitDir);unitDirection=lllUnitDir;}
-            //The other 3/4 probability of continuing the unitDirection
-            } else if (uc.canMove(unitDirection)) uc.move(unitDirection);
-        }
-        /*Otherwise move random*/
-        if (((int)(Math.random()*4)==1) || (!uc.canMove(unitDirection))){
-            if (uc.canMove(rUnitDir) && (int)(Math.random()*2)==1) {uc.move(rUnitDir);unitDirection=rUnitDir;}
-            else if (uc.canMove(lUnitDir)) {uc.move(lUnitDir);unitDirection=lUnitDir;}
-            //If not able to go: turn right or left 90º
-            else if ((int)(Math.random()*2)==1 && uc.canMove(rrUnitDir)) {uc.move(rrUnitDir);unitDirection=rrUnitDir;}
-            else if ((int)(Math.random()*2)==1 && uc.canMove(llUnitDir)) {uc.move(llUnitDir);unitDirection=llUnitDir;}
-
-            //If not able to go: turn right or left º
-            else if ((int)(Math.random()*2)==1 && uc.canMove(rrrUnitDir)) {uc.move(rrrUnitDir);unitDirection=rrrUnitDir;}
-            else if ((int)(Math.random()*2)==1 && uc.canMove(lllUnitDir)) {uc.move(lllUnitDir);unitDirection=lllUnitDir;}
-        //The other 3/4 probability of continuing the unitDirection
-        } else if (uc.canMove(unitDirection)) uc.move(unitDirection);
+            if(uc.canMove(targetDir))checkedDir = targetDir;
+            else checkedDir = goDirection(targetDir);
+        } //Second options: keep direction
+        else if(computeDirection() != null) checkedDir = goDirection(averageDir(unitDirection, computeDirection()));
+        else checkedDir = goDirection(unitDirection);
         /*If there is no target, also move random*/
-        //else if (uc.canMove(dir)) uc.move(dir);
+        /*If there is no target, also move random*/
+        unitDirection = checkedDir;
+        if (uc.canMove(checkedDir)) uc.move(unitDirection);
 
         uc.write(myLoc, encodeLocation(uc.getLocation())); // Marca on està localitzat
-        uc.write(myLoc-100, 1); // Marca que continua viu
+        uc.write(myLoc-100, 2); // Marca que continua viu
+    }
+    Direction averageDir(Direction a, Direction b){
+        return  Direction.getDirection((int)(a.dx+b.dx), (int)(a.dy+b.dy));
     }
 
 
+    Direction goDirection (Direction dir){
+        Direction rDir = dir.rotateRight(); Direction rrDir = rDir.rotateRight(); Direction rrrDir = rrDir.rotateRight();
+        Direction lDir = dir.rotateLeft(); Direction llDir = lDir.rotateLeft(); Direction lllDir = llDir.rotateLeft();
 
-
+        //0.25 probability of turning diagonally
+        if ((int)(Math.random()*4)==1) {
+            //Choose left or right direction
+            if ((int)(Math.random()*2)==1)
+                if (uc.canMove(rDir)) dir = rDir;
+                else if (uc.canMove(lDir)) dir = lDir;
+                else if (uc.canMove(lDir)) dir = lDir;
+                else if (uc.canMove(rDir)) dir = rDir;
+        } else if(!uc.canMove(dir)){
+            if ((int)(Math.random()*2)==1) {
+                if (uc.canMove(rrDir)) {
+                    dir = rrDir;
+                } else if (uc.canMove(llDir)) {
+                    dir = llDir;
+                }
+            } else if (uc.canMove(llDir)) {
+                dir = llDir;
+            } else if (uc.canMove(rrDir)) {
+                dir = rrDir;
+            } else if ((int)(Math.random()*2)==1) {
+                if (uc.canMove(rrrDir)) {
+                    dir = rrrDir;
+                } else if (uc.canMove(lllDir)) {
+                    dir = lllDir;
+                }
+            } else if (uc.canMove(lllDir)) {
+                dir = lllDir;
+            } else if (uc.canMove(rrrDir)) {
+                dir = rrrDir;
+            }
+        }
+        return dir;
+    }
 
 
 
@@ -114,12 +121,12 @@ public class Pitcher extends MyUnit {
     }
 
     Direction closeBatters(UnitController uc){
-        float myVision = uc.getType().getStat(UnitStat.VISION_RANGE);
+        float myVision = uc.getType().getStat(UnitStat.VISION_RANGE)/2;
         UnitInfo[] enemies = uc.senseUnits(myVision, uc.getOpponent());
-        
+
         return batterDirection(uc, enemies, UnitType.BATTER);
     }
-    
+
     Direction batterDirection(UnitController uc, UnitInfo[] enemies, UnitType unitType){
         for (UnitInfo enemy : enemies){
             UnitInfo unit = uc.senseUnitAtLocation(enemy.getLocation());
@@ -141,4 +148,41 @@ public class Pitcher extends MyUnit {
         }
         return null;
     }
+
+    Direction computeDirection(){
+        Location loc = uc.getLocation();
+        Location[] possibleLocations = new Location[8];
+        possibleLocations[0] = new Location(loc.x+4, loc.y);//EAST
+        possibleLocations[1] = new Location(loc.x, loc.y-4);//SOUTH
+        possibleLocations[2] = new Location(loc.x-4, loc.y);//WEST
+        possibleLocations[3] = new Location(loc.x, loc.y+4);//NORTH
+        int counter = 0;
+        int[] possibilities = new int[] {1,1,1,1};
+        for (int i=0; i<4; i++){
+            if (uc.isOutOfMap(possibleLocations[i])){
+                possibilities[i]=0;
+                counter += 1; // Obs: can't be >2
+            }
+        }
+        Direction result;
+        if (counter == 2){
+            if(possibilities[0]==1){
+                if(possibilities[1]==1) return Direction.SOUTHEAST;
+                if(possibilities[3]==1) return Direction.NORTHEAST;
+                return Direction.EAST;
+            } if(possibilities[2]==1){
+                if(possibilities[1]==1) return Direction.SOUTHWEST;
+                if(possibilities[3]==1) return Direction.NORTHWEST;
+                return Direction.WEST;
+            } if(possibilities[1]==1) return Direction.SOUTH;
+            return Direction.NORTH;
+        } else {
+            if (possibilities[0]==0) return Direction.WEST;
+            if (possibilities[1]==0) return Direction.NORTH;
+            if (possibilities[2]==0) return Direction.EAST;
+            if (possibilities[3]==0) return Direction.SOUTH;
+        }
+        return null;
+    }
+
 }
